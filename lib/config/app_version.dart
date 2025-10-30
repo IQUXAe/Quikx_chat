67c17cb18a2d60e0be2a6e07d61761a61202bad1
IQUXAe <iquxae@users.noreply.github.com>
2025-10-30 17:55:25 +0300

    chore: Bump version to 1.0.0+6 (beta2)
    - Update build number from 5 to 6
    - Update release tag to 1.0-beta2-301025

--- lib/config/app_version.dart
+++ lib/config/app_version.dart
@@ -1,6 +1,6 @@
 class AppVersion {
   static const String version = '1.0.0';
-  static const String buildNumber = '5';
+  static const String buildNumber = '6';
   static const String fullVersion = '$version+$buildNumber';
-  static const String releaseTag = '1.0-beta1-271025';
+  static const String releaseTag = '1.0-beta2-301025';
 }
