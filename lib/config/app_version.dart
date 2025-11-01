11f527bceae46b73f435b62eef602a5aff1c7e30
IQUXAe <iquxae@users.noreply.github.com>
2025-11-01 10:51:09 +0300

    chore: Update version and cleanup
    - Update app version
    - Remove unused build_with_secrets.sh script
    - Update pubspec.yaml

--- lib/config/app_version.dart
+++ lib/config/app_version.dart
@@ -1,6 +1,6 @@
 class AppVersion {
   static const String version = '1.0.0';
-  static const String buildNumber = '6';
+  static const String buildNumber = '7';
   static const String fullVersion = '$version+$buildNumber';
-  static const String releaseTag = '1.0-beta2-301025';
+  static const String releaseTag = '1.0-beta3-011125';
 }
